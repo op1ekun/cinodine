4588151d2fb00bc9c5cdf5bc2743019b444db137
op1ekun <woociek@gmail.com>
2012-06-18 07:10:20 +0200

    folder structure of repo, some initial coding

---
+++ t/fake.test.js
@@ -0,0 +1,15 @@
+//fake tests for mockups
+module("mockup", {
+	setup: function(){
+		
+	},
+	teardown: function(){
+		
+	}
+});
+
+test("some fake test case", function() {
+	expect(1);
+	
+	ok(false, "this is not true!");
+});
