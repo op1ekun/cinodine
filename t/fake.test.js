e3b059fbf4e2e9606c9859ffe5d24b337bac93b4
op1ekun <woociek@gmail.com>
2012-06-25 18:12:13 +0200

    qunit added dependency, obselete files removed, fake.test works in both browser and node.js

--- t/fake.test.js
+++ t/fake.test.js
@@ -1,15 +1,14 @@
+module = QUnit.module;
+
 //fake tests for mockups
-module("mockup", {
-	setup: function(){
-		
-	},
-	teardown: function(){
-		
-	}
-});
+module("mockup");
 
 test("some fake test case", function() {
 	expect(1);
 	
 	ok(false, "this is not true!");
+});
+
+test("some DOM test", function() {
+		
 });
