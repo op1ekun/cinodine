9bf3bf3a59b20c7ddc8ee6a7f308808308d6365c
op1ekun <woociek@gmail.com>
2012-07-26 23:57:43 +0200

    test paths moved to config.js, logs reset afetr each testrunner.run()

--- config.js
+++ config.js
@@ -1,5 +1,15 @@
 exports.paths   = [
-    // codes : tests
-    { './mocks/'            : './t' },
-    { '../LocalCache/lib/'  : '../LocalCache/t/' }    
+    {
+        code    : "./mocks/mock.js",
+        tests   : "./t/mock.test.js"   
+    },
+    {
+        code    : "../LocalCache/lib/Storage.js",
+        tests   : "../LocalCache/t/Storage.test.js"
+    },
+    {
+        deps    : "../LocalCache/lib/Storage.js",
+        code    : "../LocalCache/lib/Cache.js",
+        tests   : "../LocalCache/t/Cache.test.js"
+    }
 ];
